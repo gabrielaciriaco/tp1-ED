6b48ae894a01475725d321cba7d4f36b248f4a03
Gabriela Ciriaco <gabs.ciriaco@gmail.com>
2021-02-07 17:58:34 -0300

    leitura do arquivo de comandos

--- src/Controlador.cpp
+++ src/Controlador.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include "../include/Controlador.h"
 
-Controlador::Controlador(char** mapa){
+Controlador::Controlador(char **mapa){
     for(int i=0;i<50;i++){
         robos[i] = Robo();
     }
--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,88 @@
 #include <iostream>
+#include <fstream>
+#include "../include/Controlador.h"
 
 using namespace std;
 
-int main(int argc, char *argv[])
+int contaQuantidadeDeComandos(char *nomeArquivo)
 {
-    char *nomeArquivo = argv[1];    
     FILE *arquivo = fopen(nomeArquivo, "r");
     if (arquivo == NULL)
     {
         printf("Arquivo no encontrado, verifique se o arquivo esta na pasta");
         exit(1);
     }
-    
+    int quantidadeDeComandos = 0;
+    char ch;
+    while ((ch = fgetc(arquivo)) != EOF)
+    {
+        if (ch == '\n')
+        {
+            quantidadeDeComandos++;
+        }
+    }
+    fclose(arquivo);
+    return quantidadeDeComandos;
+}
+string **lerComandos(char *nomeArquivo)
+{
+    int quantidadeDeComandos = contaQuantidadeDeComandos(nomeArquivo);
+    string **comandos = new string *[quantidadeDeComandos];
+    FILE *arquivo = fopen(nomeArquivo, "r");
+    if (arquivo == NULL)
+    {
+        printf("Arquivo no encontrado, verifique se o arquivo esta na pasta");
+        exit(1);
+    }
+    char aux;
+    for (int i = 0; i < quantidadeDeComandos; i++)
+    {
+        comandos[i] = new string[4];
+        for (int j = 0; j < 2; j++)
+        {
+            string trechoComando = "";
+            do
+            {
+                fscanf(arquivo, "%c", &aux);
+                while (aux != ' ' && aux != '\n')
+                {
+                    trechoComando += aux;
+                    fscanf(arquivo, "%c", &aux);
+                }
+            } while (trechoComando == "");
+
+            comandos[i][j] = trechoComando;
+        }
+        fscanf(arquivo, "%c", &aux);
+
+        if (aux != '(')
+        {
+            fseek(arquivo, -1, SEEK_CUR);
+            fscanf(arquivo, "%*[\n]");
+            continue;
+        }
+        fscanf(arquivo, "%c,", &aux);
+        comandos[i][2] += aux;
+        fscanf(arquivo, "%c", &aux);
+        comandos[i][3] += aux;
+
+        while (fgetc(arquivo) != '\n')
+        {
+        }
+    }
+    fclose(arquivo);
+    return comandos;
+}
+int main(int argc, char *argv[])
+{
+    char *nomeArquivo = argv[1];
+    FILE *arquivo = fopen(nomeArquivo, "r");
+    if (arquivo == NULL)
+    {
+        printf("Arquivo no encontrado, verifique se o arquivo esta na pasta");
+        exit(1);
+    }
+
     int quantidadeDeLinhasDoMapa;
     int quantidadeDeColunasDoMapa;
     fscanf(arquivo, "%d", &quantidadeDeLinhasDoMapa);
@@ -21,15 +92,21 @@ int main(int argc, char *argv[])
     cout << quantidadeDeLinhasDoMapa << endl;
     cout << quantidadeDeColunasDoMapa;
 
-    char mapa[quantidadeDeLinhasDoMapa][quantidadeDeColunasDoMapa];
+    char **mapa = new char *[quantidadeDeLinhasDoMapa];
 
     for (int i = 0; i < quantidadeDeLinhasDoMapa; i++)
     {
+        mapa[i] = new char[quantidadeDeColunasDoMapa];
+
         for (int j = 0; j < quantidadeDeColunasDoMapa; j++)
         {
             fscanf(arquivo, "%c ", &mapa[i][j]);
         }
         fscanf(arquivo, "%*[\n]");
     }
+    fclose(arquivo);
+    Controlador controlador = Controlador(mapa);
+
+    string **comandos = lerComandos(argv[2]);
     return 0;
 }
